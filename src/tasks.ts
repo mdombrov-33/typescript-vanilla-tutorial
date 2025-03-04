e30cbd58cc042f69f352e9119545845dd4d04a42
Maksym Dombrov <maksymdombrov@gmail.com>
2025-03-04 14:52:50 +0200

    tasks project 2 end

--- src/tasks.ts
+++ src/tasks.ts
@@ -8,14 +8,30 @@ type Task = {
   isCompleted: boolean;
 };
 
-const tasks: Task[] = [];
+const tasks: Task[] = loadTasks();
+
+tasks.forEach(renderTask);
+
+function loadTasks(): Task[] {
+  const storedTasks = localStorage.getItem("tasks");
+  return storedTasks ? JSON.parse(storedTasks) : [];
+}
 
 function createTask(e: SubmitEvent) {
   // we have to explicitly write the type of the event if we doing a reference in .addEventListener
   e.preventDefault();
   const taskDescription = formInput?.value;
   if (taskDescription) {
-    console.log(taskDescription);
+    const task: Task = {
+      description: taskDescription,
+      isCompleted: false,
+    };
+    //add task to list
+    addTask(task);
+    //render tasks
+    renderTask(task);
+    //update localstorage
+    updateStorage();
     formInput.value = "";
     return;
   }
@@ -23,3 +39,28 @@ function createTask(e: SubmitEvent) {
 }
 
 taskForm?.addEventListener("submit", createTask);
+
+function addTask(task: Task): void {
+  tasks.push(task);
+  console.log(tasks);
+}
+
+function renderTask(task: Task): void {
+  const taskElement = document.createElement("li");
+  taskElement.textContent = task.description;
+  // checkbox
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = task.isCompleted;
+  checkbox.addEventListener("change", () => {
+    task.isCompleted = !task.isCompleted;
+    updateStorage();
+  });
+
+  taskElement.appendChild(checkbox);
+  taskListElement?.appendChild(taskElement);
+}
+
+function updateStorage(): void {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
